e277f95915edda04ce1b8ce5e9e8175e7e4f65be
Pravinkumar0908 <142499168+Pravinkumar0908@users.noreply.github.com>
2024-08-15 00:28:15 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWPPT6eIYtnXoTBUlo3CMLl4M2TQJeFoM",
-  authDomain: "ecommerce-34f94.firebaseapp.com",
-  projectId: "ecommerce-34f94",
-  storageBucket: "ecommerce-34f94.appspot.com",
-  messagingSenderId: "281987588521",
-  appId: "1:281987588521:web:YOUR_SPECIFIC_APP_ID"
+  apiKey: "",
+  authDomain: ".firebaseapp.com",
+  projectId: "",
+  storageBucket: ".appspot.com",
+  messagingSenderId: "",
+  appId: "1::web:YOUR_SPECIFIC_APP_ID"
 };
 
 // Initialize Firebase
